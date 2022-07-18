cd3edd12945ace6601d03e08d285890b7aa1bab7
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-07-18 12:44:29 -0300

    add RestTemplate by Bean

--- src/main/java/br/com/devcanoa/moviesticker/imdb/ImdbClient.java
+++ src/main/java/br/com/devcanoa/moviesticker/imdb/ImdbClient.java
@@ -1,6 +1,8 @@
 package br.com.devcanoa.moviesticker.imdb;
 
 import br.com.devcanoa.moviesticker.exception.ImdbClientException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,9 +12,16 @@ import java.util.Objects;
 @Service
 public class ImdbClient {
 
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public ImdbClient(@Qualifier("restTemplate") RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     public String getBody() {
         try {
-            return Objects.requireNonNullElse(new RestTemplate().getForObject(getUri(), String.class), "");
+            return Objects.requireNonNullElse(restTemplate.getForObject(getUri(), String.class), "");
         } catch (Exception e) {
             throw new ImdbClientException(e.getMessage());
         }
