28b1660e4ae01bb0a33a52b15a0fdff138b30e9d
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-07-18 12:45:28 -0300

    add tests with mock

--- src/test/java/br/com/devcanoa/moviesticker/MovieStickerApplicationTests.java
+++ src/test/java/br/com/devcanoa/moviesticker/MovieStickerApplicationTests.java
@@ -3,11 +3,15 @@ package br.com.devcanoa.moviesticker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Objects;
+
 @SpringBootTest
 class MovieStickerApplicationTests {
-
-    @Test
-    void contextLoads() {
+    protected String getStringWithTwoMovies() {
+        try {
+            return new String(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("twoMovies.txt")).readAllBytes());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("File not found");
+        }
     }
-
 }
