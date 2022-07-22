066d2ebc45bf3ef274a56f7de2f2af8ee074e9af
Lucas Ramon Pinheiro Serrao <lucas.serrao@b2wdigital.com>
2022-07-22 15:19:10 -0300

    adjust heroku deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/movie-sticker-0.0.1.jar
+web: java -Dserver.port=$PORT $JAVA_OPTS -jar build/libs/sticker-maker-0.0.1.jar
--- src/test/java/br/com/devcanoa/stickermaker/MovieStickerApplicationTests.java
+++ src/test/java/br/com/devcanoa/stickermaker/MovieStickerApplicationTests.java
@@ -1,16 +1,11 @@
 package br.com.devcanoa.stickermaker;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Objects;
 
 @SpringBootTest
 class MovieStickerApplicationTests {
-    protected String getStringWithTwoMovies() {
-        try {
-            return new String(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("twoMovies.txt")).readAllBytes());
-        } catch (Exception e) {
-            throw new IllegalArgumentException("File not found");
-        }
+    @Test
+    void contextLoads() {
     }
 }
